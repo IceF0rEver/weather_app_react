037b96895c0cec8da0a280be786240b67cfc9f96
IceF0rEver <arnaudorban.pro@gmail.com>
2025-04-30 14:37:35 +0200

    feat: homePage components

---
+++ src/components/Current.tsx
@@ -0,0 +1,90 @@
+import { useState, useEffect, useContext } from 'react';
+import { useFetch } from '../hooks/useFetch';
+import { useLocalStorage } from '../hooks/useLocalStorage';
+import { CurrentCoordContext } from '../App';
+import { LocalStorageData } from '../types/localStorage';
+import Map from './Map';
+
+export function Current() {
+    const apiKey = import.meta.env.VITE_API_KEY;
+    
+    const [url, setUrl] = useState('');
+    const [currentData, setCurrentData] = useState<LocalStorageData>();
+    
+	const current = useContext(CurrentCoordContext);
+    const { fetchDatas } = useFetch({ url });
+
+
+    useEffect(() => {
+        if (current && current.latitude !== 0 && current.longitude !== 0) {
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${current.latitude}&lon=${current.longitude}&appid=${apiKey}&lang=fr&units=metric`;
+            setUrl(url);
+        }
+    }, [current]);
+
+    useEffect(() => {
+        if (fetchDatas && current){
+            setCurrentData({
+                latitude: current.latitude,
+                longitude: current.longitude,
+                city: fetchDatas.name,
+                country: fetchDatas.sys.country,
+                current: true,
+                isActive: true,
+            })
+        }
+    }, [fetchDatas]);
+    
+    useLocalStorage({
+        action: 'add',
+        key: 'current',
+        data: currentData,
+    });    
+
+
+    return (
+        <div>
+            <section className="bg-white dark:bg-gray-900 dark:bg-opacity-80 bg-opacity-75 rounded-lg shadow p-5 flex flex-col">
+                <header className="mb-4">
+                    <h2 className="font-bold text-xl dark:text-gray-50">Météo instantanée</h2>
+                </header>
+                <article className="flex items-center justify-between">
+                    <div>
+                        <h3 className="text-sm sm:text-lg font-semibold dark:text-gray-50">{fetchDatas?.name}</h3>
+                        {fetchDatas?.dt && (
+                            <time
+                                className="text-xs sm:text-sm text-gray-600 dark:text-gray-300"
+                                dateTime={new Date(fetchDatas.dt * 1000).toISOString()}
+                            >
+                                {new Date(fetchDatas.dt * 1000).toLocaleTimeString('fr', {
+                                hour: '2-digit',
+                                minute: '2-digit',
+                                })}
+
+                                <br />
+
+                                {new Date(fetchDatas.dt * 1000).toLocaleDateString('fr', {
+                                day: 'numeric',
+                                month: 'short',
+                                year: 'numeric',
+                                })}
+                            </time>
+                        )}
+                    </div>
+                    <div className="flex gap-2">
+                        <div className="text-right my-auto">
+                            <p className="text-2xl sm:text-5xl dark:text-gray-50">{ fetchDatas?.main.temp.toFixed(0) }°C</p>
+                            <p className="text-sm sm:text-lg text-gray-600 dark:text-gray-300">{ fetchDatas?.weather[0].description }</p>
+                        </div>
+                        <div className="my-auto w-2/3 sm:w-full">
+                            <img className="mx-auto my-auto bg-blue-300 rounded-full" src={`https://openweathermap.org/img/wn/${fetchDatas?.weather[0].icon}@2x.png`} alt="img"/>
+                        </div>
+                    </div>
+                </article>
+                 <article className="pt-4">
+                 <Map />
+                 </article>
+            </section>
+        </div>
+    )
+};
---
+++ src/components/Hourly.tsx
@@ -0,0 +1,103 @@
+import { useState, useEffect, useContext } from 'react';
+import { useFetch } from '../hooks/useFetch';
+import { CurrentCoordContext } from '../App';
+import { LineChartComponent } from './LineChart';
+
+export function Hourly() {
+    const apiKey = import.meta.env.VITE_API_KEY;
+
+    interface Item {
+        dt: number;
+        temp_min: number;
+        temp_max: number;
+        humidity: number;
+        icon: string;
+    };
+    
+    interface ItemChart {
+        dt: number;
+        temp: number;
+    };
+        
+    const [url, setUrl] = useState('');
+    const [items, setItems] = useState<Item[]>([]);
+    const [itemsChart, setItemsChart] = useState<ItemChart[]>([]);
+        
+    const current = useContext(CurrentCoordContext);
+    const { fetchDatas } = useFetch({ url });
+    
+    
+    useEffect(() => {
+        if (current && current.latitude !== 0 && current.longitude !== 0) {
+            const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${current.latitude}&lon=${current.longitude}&appid=${apiKey}&lang=fr&units=metric`;
+            setUrl(url);
+        }
+    }, [current]);
+
+    useEffect(() => {
+        if (fetchDatas) {
+            for (const item of fetchDatas.list.slice(0,10)) {
+                setItems(prev => [
+                    ...prev,
+                    {
+                        dt: item.dt,
+                        temp_min: Number(item.main.temp_min.toFixed(0)),
+                        temp_max: Number(item.main.temp_max.toFixed(0)),
+                        humidity: Number(item.main.humidity.toFixed(0)),
+                        icon: item.weather[0].icon,
+                    }
+                ]);
+                setItemsChart(prev => [
+                    ...prev,
+                    {
+                        dt : item.dt,
+                        temp: Number(item.main.temp_max.toFixed(0)),
+                    }
+                ]);
+            }
+        }
+    }, [fetchDatas]);
+
+    return (
+        <div>
+            <section className="bg-white dark:bg-gray-900 dark:bg-opacity-80 bg-opacity-75 rounded-lg shadow p-5">
+                <h2 className="font-bold dark:text-gray-50 text-xl mb-4">Prochaines heures</h2>
+                <div>
+                    <div className=" bg-white rounded-lg p-1 py-4 overflow-x-auto">
+                        <div className=" h-72 min-w-[420px] sm:min-w-1/3 px-3">
+                            <LineChartComponent
+                                chartData={itemsChart}
+                                config={{
+                                    temp: {
+                                        label: "Températures",
+                                        color: "#808080",
+                                    }
+                                }}
+                            />
+                        </div>
+                        <div className="flex justify-between min-w-[500px] sm:min-w-1/3">
+                            { items.length > 0 && items.map(item =>
+                            <div key={item.dt}>
+                                <div className="flex content-center justify-center">
+                                    <p className="text-xs text-black">
+                                        { new Date(item.dt * 1000).toLocaleTimeString('fr', { hour: '2-digit'}) }
+                                    </p>
+                                </div>
+                                <div className="bg-blue-300 rounded-lg">
+                                    <img className="mx-auto size-12" src={`https://openweathermap.org/img/wn/${item.icon}@4x.png`} alt="weather icon"/>
+                                    <div className="flex content-center justify-center px-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
+                                        <path fill="currentColor" d="M12 4.67c-4.05 3.7-6 6.79-6 9.14c0 3.63 2.65 6.2 6 6.2s6-2.57 6-6.2c0-2.35-1.95-5.45-6-9.14m.28 14.32c-2.13.13-4.62-1.09-5.19-4.12a.75.75 0 0 1 1.48-.25c.41 2.23 2.28 2.98 3.64 2.87c.43-.02.79.32.79.75c0 .4-.32.73-.72.75" opacity=".3"/><path fill="currentColor" d="M12 2c-5.33 4.55-8 8.48-8 11.8c0 4.98 3.8 8.2 8 8.2s8-3.22 8-8.2c0-3.32-2.67-7.25-8-11.8m0 18c-3.35 0-6-2.57-6-6.2c0-2.34 1.95-5.44 6-9.14c4.05 3.7 6 6.79 6 9.14c0 3.63-2.65 6.2-6 6.2m-4.17-6c.37 0 .67.26.74.62c.41 2.22 2.28 2.98 3.64 2.87c.43-.02.79.32.79.75c0 .4-.32.73-.72.75c-2.13.13-4.62-1.09-5.19-4.12a.75.75 0 0 1 .74-.87"/>
+                                    </svg>
+                                        <p className="text-xs pb-2 text-black">{ item.humidity }%</p>
+                                    </div>
+                                </div>
+                            </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
