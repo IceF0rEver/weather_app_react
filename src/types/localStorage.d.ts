141fce8b81a017a9a998fdd14217774c287f2223
IceF0rEver <arnaudorban.pro@gmail.com>
2025-05-05 19:37:10 +0200

    fix: syntaxe

--- src/types/localStorage.d.ts
+++ src/types/localStorage.d.ts
@@ -1,8 +1,8 @@
 export interface LocalStorageData {
+    id: number;
     latitude: number;
     longitude: number;
     city: string;
     country: string;
     current: boolean;
-    isActive: boolean;
   }
